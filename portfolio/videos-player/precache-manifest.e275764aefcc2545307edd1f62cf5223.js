839b750dfbe284e198545e2f227e13c975de059b
Riko Trihendrawan <riko.trihendrawan@gmail.com>
2022-04-06 08:36:33 +0700

    Updated reactjs YT videos player
    Initial appearance now with loaded videos

---
+++ portfolio/videos-player/precache-manifest.e275764aefcc2545307edd1f62cf5223.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c273911ffcd75832b6522bcd986c4052",
+    "url": "/portfolio/videos-player/index.html"
+  },
+  {
+    "revision": "8450e8567f8fd51a188f",
+    "url": "/portfolio/videos-player/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "898936945d8d0262fb20",
+    "url": "/portfolio/videos-player/static/css/main.af391da0.chunk.css"
+  },
+  {
+    "revision": "8450e8567f8fd51a188f",
+    "url": "/portfolio/videos-player/static/js/2.c3d6e4d4.chunk.js"
+  },
+  {
+    "revision": "898936945d8d0262fb20",
+    "url": "/portfolio/videos-player/static/js/main.135d3335.chunk.js"
+  },
+  {
+    "revision": "651fa9c9bde6a6c223bb",
+    "url": "/portfolio/videos-player/static/js/runtime~main.56cd1bea.js"
+  }
+]);
--- portfolio/videos-player/service-worker.js
+++ portfolio/videos-player/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/videos-player/precache-manifest.e54e0fc360744f1f69932375aa4aa574.js"
+  "/portfolio/videos-player/precache-manifest.e275764aefcc2545307edd1f62cf5223.js"
 );
 
 self.addEventListener('message', (event) => {
