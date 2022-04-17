d9b312e8952d18e5a9d4b5a1a647b3457fd9e9bd
Riko Trihendrawan <riko.trihendrawan@gmail.com>
2022-04-17 18:42:08 +0700

    Updated portfolio with Jobboard
    - Added portfolio item for graphql
    - Fixed deploy blank issue

---
+++ portfolio/job-board-graphql/precache-manifest.da16b21211d351d43be63c2535bf66da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f6e90f7f0e1f06983ba",
+    "url": "/portfolio/job-board-graphql/static/css/main.1dc978aa.chunk.css"
+  },
+  {
+    "revision": "5f6e90f7f0e1f06983ba",
+    "url": "/portfolio/job-board-graphql/static/js/main.5f6e90f7.chunk.js"
+  },
+  {
+    "revision": "90fc6e6cd8e595e84f5e",
+    "url": "/portfolio/job-board-graphql/static/js/1.90fc6e6c.chunk.js"
+  },
+  {
+    "revision": "af98b1ab157840f44aaa",
+    "url": "/portfolio/job-board-graphql/static/js/runtime~main.af98b1ab.js"
+  },
+  {
+    "revision": "262b6c17ed99e3cfa0a73d5abf2af7a7",
+    "url": "/portfolio/job-board-graphql/index.html"
+  }
+];
--- portfolio/job-board-graphql/service-worker.js
+++ portfolio/job-board-graphql/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f65927810c5b8daa80bc6009d2c52b24.js"
+  "/portfolio/job-board-graphql/precache-manifest.da16b21211d351d43be63c2535bf66da.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio/job-board-graphql/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
